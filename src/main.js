0b64f865223926e68912bc8e63e7eb27e8a6f3ce
João Cavalcante <contato@kavalcante.com>
2018-04-19 01:17:51 -0300

    Playlist Trigger Done

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import SearchTrigger from './SearchTrigger';
 import SelectAlbumTrigger from './SelectAlbumTrigger';
+import PlaylistTrigger from './PlaylistTrigger';
 
 SearchTrigger();
 SelectAlbumTrigger();
+PlaylistTrigger();
